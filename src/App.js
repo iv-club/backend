f2bb4ea69396106d87f49bcfb0d1a24cb3857222
jinwoo <bluekjw3516@naver.com>
2023-08-06 19:36:33 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
   'name' : '김진우',
   'birthday' : '950521',
   'gender' : 'man',
   'job' : 'programmer'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동',
+  'birthday' : '950717',
+  'gender' : 'man',
+  'job' : 'programmer'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '이순신',
+  'birthday' : '021205',
+  'gender' : 'man',
+  'job' : '디자이너'
 }
+]
+
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      {
+        customers.map(c=> {
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.name}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })
+      }
+</div>
   );
 }
 
