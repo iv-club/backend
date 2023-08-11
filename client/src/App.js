4344c267e9c7443ffb260eaf50077d237a84fbf8
jinwoo <bluekjw3516@naver.com>
2023-08-11 16:18:19 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Customer from './components/Customer';
@@ -19,60 +20,54 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '김진우',
-  'birthday' : '950521',
-  'gender' : 'man',
-  'job' : 'programmer'
-},
-{
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '홍길동',
-  'birthday' : '950717',
-  'gender' : 'man',
-  'job' : 'programmer'
-},
-{
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '이순신',
-  'birthday' : '021205',
-  'gender' : 'man',
-  'job' : '디자이너'
-}
-]
+class App extends Component { // class 컴포넌트
 
-function App() {
-  const {classes} = this.props;
-  return (
-    <div>
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-        {customers.map(c=> { return( <Customer key={c.id} id={c.id} image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}/>);})}
-          </TableBody>
-        </Table>
-      </Paper>
-</div>
-  );
+  state = { // 변경될수 있는 변수 그러나 props는 변경될 수 없는 변수를 의미 
+    customers: ""
+  }
+
+  componentDidMount() { // API 서버에서 데이터를 받아올 수 있는 작업을 처리
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => { // 비동기적으로 수행
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render(){
+    const {classes} = this.props;
+    return (
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+          {this.state.customers ? this.state.customers.map(c=> { return( <Customer key={c.id} id={c.id} image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}/>);
+                }) : ""}
+            </TableBody>
+          </Table>
+        </Paper>
+  </div>
+    );
+  }
+  
 }
 
 export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req,res) => {
+    res.send([
+        {
+        'id' : 1,
+        'image' : 'https://placeimg.com/64/64/1',
+        'name' : '김진우',
+        'birthday' : '950521',
+        'gender' : 'man',
+        'job' : 'programmer'
+      },
+      {
+        'id' : 2,
+        'image' : 'https://placeimg.com/64/64/2',
+        'name' : '홍길동',
+        'birthday' : '950717',
+        'gender' : 'man',
+        'job' : 'programmer'
+      },
+      {
+        'id' : 3,
+        'image' : 'https://placeimg.com/64/64/3',
+        'name' : '이순신',
+        'birthday' : '021205',
+        'gender' : 'man',
+        'job' : '디자이너'
+      }
+    ])
 });
 
-app.listen(port, () => console.log(`Listening on  port ${port}`));
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
